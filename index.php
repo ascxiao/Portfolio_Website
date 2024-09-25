ede1b1c7c9c995eb85ad6ff72134da8d600500e0
Carlos Valderrama <81756261+ascxiao@users.noreply.github.com>
2024-09-25 10:32:16 +0800

    new stuff

--- index.php
+++ index.php
@@ -6,14 +6,27 @@
     ?>
 
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="">
 
     <title><?php echo $title; ?></title>
+
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- Change icon later -->
+    <link rel="icon" href="./assets/favicon.ico"> 
+    
+    <link href = "style.css" rel = "stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+
 </head>
 
 <body>
-    <header id="top">
+    <?php include('menu.php')?>
+
+  
+   <header id="top">
         <div class="left">
             <a href = "index.php"><img src="./assets/icon.png" alt="Kaluroos Linnaeus Logo"></a>
             <div class="titleLogo">
@@ -97,6 +110,9 @@
             <a href="#top">Back to top</a>
         </div>
     </footer>
-</body>
 
+    <script src="./ie-emulation-modes-warning.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
 </html>
